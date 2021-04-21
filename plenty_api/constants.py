f714388f2dff6fb43bd9c5c0c3eec51b9ae699d0
Peter Hahn <peter.hahn@initos.com>
2021-04-21 12:04:56 +0200

    Add more GET routes
    Add additional GET routes to pull stock data, storage locations,
    warehouses and contacts.
    Provide additional public methods to pull storage locations for a
    specific variation.
    
    Signed-off-by: Peter Hahn <peter.hahn@initos.com>
    Reviewed-by: Sebastian Fricke <sebastian.fricke@posteo.net>
    Signed-off-by: Sebastian Fricke <sebastian.fricke@posteo.net>

--- plenty_api/constants.py
+++ plenty_api/constants.py
@@ -22,14 +22,33 @@
     https://developers.plentymarkets.com/rest-doc#
 """
 
-VALID_DOMAINS = ['order', 'item', 'variation', 'vat', 'prices', 'manufacturer',
-                 'attribute', 'referrer', 'redistribution']
-VALID_ROUTES = ['/rest/orders', '/rest/items', '/rest/items/variations',
-                '/rest/vat', '/rest/items/sales_prices',
-                '/rest/items/manufacturers', '/rest/items/attributes',
-                '/rest/orders/referrers', '/rest/redistributions']
-DOMAIN_ROUTE_MAP = {VALID_DOMAINS[i]: VALID_ROUTES[i]
-                    for i in range(len(VALID_DOMAINS))}
+VALID_DOMAINS = ['order',
+                 'item',
+                 'variation',
+                 'vat',
+                 'prices',
+                 'manufacturer',
+                 'attribute',
+                 'referrer',
+                 'stockmanagement',
+                 'warehouses',
+                 'contact',
+                 'redistribution'
+                 ]
+VALID_ROUTES = ['/rest/orders',
+                '/rest/items',
+                '/rest/items/variations',
+                '/rest/vat',
+                '/rest/items/sales_prices',
+                '/rest/items/manufacturers',
+                '/rest/items/attributes',
+                '/rest/orders/referrers',
+                '/rest/stockmanagement/stock',
+                '/rest/stockmanagement/warehouses',
+                '/rest/accounts/contacts',
+                '/rest/redistributions'
+                ]
+DOMAIN_ROUTE_MAP = dict(zip(VALID_DOMAINS, VALID_ROUTES))
 
 # Mapping of date_type function parameter value to query parameter
 # the date_type function parameter is supposed to be more descriptive
@@ -67,7 +86,23 @@ VALID_REFINE_KEYS = {
     ],
     'manufacturer': [
         'name'
-    ]
+    ],
+    'stockmanagement': [
+        'variationId',
+    ],
+    'warehouses': [
+        'variationId',
+    ],
+    'contact': [
+        'fullText', 'contactEmail', 'email', 'postalCode', 'plentyId',
+        'externalId', 'number', 'typeId', 'rating', 'createdAtBefore',
+        'createdAtAfter', 'updatedAtBefore', 'updatedAtAfter',
+        'lastOrderAtBefore', 'lastOrderAtAfter', 'with',
+        'newsletterAllowanceAfter', 'newsletterAllowanceBefore',
+        'newsletterAllowance', 'contactId', 'contactAddress', 'countryId',
+        'userId', 'referrerId', 'name', 'nameOrId', 'town', 'privatePhone',
+        'billingAddressId', 'deliveryAddressId', 'tagIds'
+    ],
 }
 
 # Valid additional argument values for GET requests, which are used to
