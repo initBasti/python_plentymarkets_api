initBasti/python_plentymarkets_api

commit 5f8ab60f4894c975046e6b0cdbd1364f6c8275ee

Feature & Refactor: Item data & generalize logic
Generalize the logic for building an API request query, to make it
usable for the item request.
Wrap multiple API requests to get data from pages into a single
data-structure.
Add the get_items public method, which applies minimal abstraction upon
the actual request.

Signed-off-by: Sebastian Fricke <sebastian.fricke.linux@gmail.com>

plenty_api/api.py
@@ -18,9 +18,9 @@
     along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
 
+from typing import List
 import requests
 import simplejson
-from typing import List
 
 import plenty_api.keyring
 import plenty_api.utils as utils
@@ -59,6 +59,14 @@ class PlentyApi():
 
                 Reference:
                 (developers.plentymarkets.com/rest-doc#/Accounting/get_rest_vat)
+            ___
+            **plenty_api_get_items**
+                Generic interface to item data from Plentymarkets with little
+                abstraction.
+                [refine]        -   Apply filters to the request
+                [additional]    -   Add additional elements to the response.
+                [last_update]   -   Date of the last update
+                [lang]          -   Provide the text within a specific language
             ___
     """
     def __init__(self, base_url, use_keyring=True, data_format='json',
@@ -103,7 +111,7 @@ class PlentyApi():
         response = requests.post(endpoint, params=creds)
         if response.status_code == 403:
             print("ERROR: Login to API failed, your account is locked")
-            print("unlock @ Setup->settings->accounts->go to user->unlock login")
+            print("unlock @ Setup->settings->accounts->{user}->unlock login")
         try:
             token = utils.build_login_token(response_json=response.json())
         except KeyError as err:
@@ -162,6 +170,42 @@ class PlentyApi():
 
         return response
 
+    def __repeat_get_request_for_all_records(self,
+                                             domain: str,
+                                             query: str) -> dict:
+        """
+            Collect data records from multiple API requests in a single JSON
+            data structure.
+
+            Parameter:
+                domain [str]        -   Orders/Items/..
+                query  [str]        -   Additional options for the request
+
+            Return:
+                [dict]              -   API response in as javascript object
+                                        notation
+        """
+        response = self.__plenty_api_request(method='get',
+                                             domain=domain,
+                                             query=query)
+        if not response:
+            return None
+
+        entries = response['entries']
+
+        while not response['isLastPage']:
+            new_query = query + str(f"&page={response['page'] + 1}")
+            response = self.__plenty_api_request(method='get',
+                                                 domain=domain,
+                                                 query=new_query)
+            if not response:
+                print(f"ERROR: subsequent {domain} API requests failed.")
+                return None
+
+            entries += response['entries']
+
+        return entries
+
     def plenty_api_get_orders_by_date(self, start, end, date_type='create',
                                       additional=None, refine=None):
         """
@@ -191,31 +235,20 @@ class PlentyApi():
 
         if not utils.check_date_range(date_range=date_range):
             print(f"ERROR: {date_range['start']} -> {date_range['end']}")
-            return None
+            return {}
 
-        query = utils.build_date_request_query(date_range=date_range,
-                                               date_type=date_type,
-                                               additional=additional,
-                                               refine=refine)
+        query_date = utils.build_query_date(date_range=date_range,
+                                            date_type=date_type)
+        query_attributes = utils.build_query_attributes(domain='orders',
+                                                        refine=refine,
+                                                        additional=additional)
+        query = utils.build_request_query(
+            elements=[query_date, query_attributes])
 
-        response = self.__plenty_api_request(method='get',
-                                             domain='orders',
-                                             query=query)
-        if not response:
-            return None
-
-        orders = response['entries']
-
-        while not response['isLastPage']:
-            new_query = query + str(f"&page={response['page'] + 1}")
-            response = self.__plenty_api_request(method='get',
-                                                 domain='orders',
-                                                 query=new_query)
-            if not response:
-                print("ERROR: subsequent API calls failed.")
-                return None
-
-            orders += response['entries']
+        orders = self.__repeat_get_request_for_all_records(domain='orders',
+                                                           query=query)
+        if not orders:
+            return {}
 
         if self.data_format == 'json':
             return orders
@@ -239,24 +272,8 @@ class PlentyApi():
             Return:
                 [JSON(Dict) / DataFrame] <= self.data_format
         """
-        response = self.__plenty_api_request(method='get',
-                                             domain='vat',
-                                             query='')
-        if not response:
-            return None
-
-        vat_data = response['entries']
-
-        while not response['isLastPage']:
-            new_query = str("?page={response['page'] + 1}")
-            response = self.__plenty_api_request(method='get',
-                                                 domain='vat',
-                                                 query=new_query)
-            if not response:
-                print("ERROR: subsequent API calls failed.")
-                return None
-
-            vat_data += response['entries']
+        vat_data = self.__repeat_get_request_for_all_records(domain='vat',
+                                                             query='')
 
         vat_table = utils.create_vat_mapping(data=vat_data, subset=subset)
 
@@ -265,3 +282,60 @@ class PlentyApi():
 
         if self.data_format == 'dateframe':
             return utils.json_to_dataframe(json=vat_table)
+
+    def plenty_api_get_items(self,
+                             refine: dict = None,
+                             additional: list = None,
+                             last_update: str = '',
+                             lang: str = ''):
+        """
+            Get product data from PlentyMarkets.
+
+            Parameter:
+                refine [dict]       -   Apply filters to the request
+                                        Example:
+                                        {'id': '12345', 'flagOne: '5'}
+                additional [list]   -   Add additional elements to the
+                                        data response.
+                                        Example:
+                                        ['variations', 'itemImages']
+                last_update [str]   -   Date of the last update given as one
+                                        of the following formats:
+                                            YYYY-MM-DDTHH:MM:SS+UTC-OFFSET
+                                            YYYY-MM-DDTHH:MM
+                                            YYYY-MM-DD
+                lang [str]          -   Provide the text within the data
+                                        in one of the following languages:
+
+                developers.plentymarkets.com/rest-doc/gettingstarted#countries
+
+            Return:
+                [JSON(Dict) / DataFrame] <= self.data_format
+        """
+        items = None
+        query_attributes = ''
+        change_date = ''
+        language = ''
+        query = ''
+
+        if refine or additional:
+            query_attributes = utils.build_query_attributes(
+                domain='items', refine=refine, additional=additional)
+        if last_update:
+            change_date = utils.date_to_timestamp(date=last_update)
+
+        if lang:
+            language = utils.get_language(lang=lang)
+
+        query = utils.build_request_query(elements=[query_attributes,
+                                                    change_date,
+                                                    language])
+
+        items = self.__repeat_get_request_for_all_records(domain='items',
+                                                          query=query)
+
+        if self.data_format == 'json':
+            return items
+
+        if self.data_format == 'dataframe':
+            return utils.json_to_dataframe(json=items)

plenty_api/utils.py
@@ -20,13 +20,14 @@
 
 import getpass
 import datetime
+import time
 import re
 import urllib.parse
 import dateutil.parser
 import pandas
 
 VALID_ROUTES = ['/rest/orders', '/rest/items', '/rest/vat']
-VALID_REFINE_KEYS = [
+VALID_ORDER_REFINE_KEYS = [
     'orderType', 'contactId', 'referrerId', 'shippingProfileId',
     'shippingServiceProviderId', 'ownerUserId', 'warehouseId',
     'isEbayPlus', 'includedVariation', 'includedItem', 'orderIds',
@@ -35,6 +36,52 @@ VALID_REFINE_KEYS = [
     'externalOrderId', 'clientId', 'paymentStatus', 'statusFrom',
     'statusTo', 'hasDocument', 'hasDocumentNumber', 'parentOrderId'
 ]
+VALID_ITEM_REFINE_KEYS = [
+    'name', 'manfacturerId', 'id', 'flagOne', 'flagTwo'
+]
+VALID_COUNTRY_MAP = {
+    "DE": 1, "AT": 2, "BE": 3, "CH": 4, "CY": 5, "CZ": 6, "DK": 7, "ES": 8,
+    "EE": 9, "FR": 10, "FI": 11, "GB": 12, "GR": 13, "HU": 14, "IT": 15,
+    "IE": 16, "LU": 17, "LV": 18, "MT": 19, "NO": 20, "NL": 21, "PT": 22,
+    "PL": 23, "SE": 24, "SG": 25, "SK": 26, "SI": 27, "US": 28, "AU": 29,
+    "CA": 30, "CN": 31, "JP": 32, "LT": 33, "LI": 34, "MC": 35, "MX": 36,
+    "IC": 37, "IN": 38, "BR": 39, "RU": 40, "RO": 41, "EA": 42,
+    "BG": 44, "XZ": 45, "KG": 46, "KZ": 47, "BY": 48, "UZ": 49, "MA": 50,
+    "AM": 51, "AL": 52, "EG": 53, "HR": 54, "MV": 55, "MY": 56, "HK": 57,
+    "YE": 58, "IL": 59, "TW": 60, "GP": 61, "TH": 62, "TR": 63,
+    "NZ": 66, "AF": 67, "AX": 68, "DZ": 69, "AS": 70, "AD": 71,
+    "AO": 72, "AI": 73, "AQ": 74, "AG": 75, "AR": 76, "AW": 77, "AZ": 78,
+    "BS": 79, "BH": 80, "BD": 81, "BB": 82, "BZ": 83, "BJ": 84, "BM": 85,
+    "BT": 86, "BO": 87, "BA": 88, "BW": 89, "BV": 90, "IO": 91,
+    "BN": 92, "BF": 93, "BI": 94, "KH": 95, "CM": 96, "CV": 97,
+    "KY": 98, "CF": 99, "TD": 100, "CL": 101, "CX": 102, "CC": 103,
+    "CO": 104, "KM": 105, "CG": 106, "CD": 107, "CK": 108, "CR": 109,
+    "CI": 110, "CU": 112, "DJ": 113, "DM": 114, "DO": 115, "EC": 116,
+    "SV": 117, "GQ": 118, "ER": 119, "ET": 120, "FK": 121, "FO": 122,
+    "FJ": 123, "GF": 124, "PF": 125, "TF": 126, "GA": 127, "GM": 128,
+    "GE": 129, "GH": 130, "GI": 131, "GL": 132, "GD": 133, "GU": 134,
+    "GT": 135, "GG": 136, "GN": 137, "GW": 138, "GY": 139, "HT": 140,
+    "HM": 141, "VA": 142, "HN": 143, "IS": 144, "ID": 145, "IR": 146,
+    "IQ": 147, "IM": 148, "JM": 149, "JE": 150, "JO": 151, "KE": 152,
+    "KI": 153, "KP": 154, "KR": 155, "KW": 156, "LA": 158, "LB": 159,
+    "LS": 160, "LR": 161, "LY": 162, "MO": 163, "MK": 164, "MG": 165,
+    "MW": 166, "ML": 168, "MH": 169, "MQ": 170, "MR": 171, "MU": 172,
+    "YT": 173, "FM": 174, "MD": 175, "MN": 176, "ME": 177, "MS": 178,
+    "MZ": 179, "MM": 180, "NA": 181, "NR": 182, "NP": 183, "AN": 184,
+    "NC": 185, "NI": 186, "NE": 187, "NG": 188, "NU": 189, "NF": 190,
+    "MP": 191, "OM": 192, "PK": 193, "PW": 194, "PS": 195, "PA": 196,
+    "PG": 197, "PY": 198, "PE": 199, "PH": 200, "PN": 201, "PR": 202,
+    "QA": 203, "RE": 204, "RW": 205, "SH": 206, "KN": 207, "LC": 208,
+    "PM": 209, "VC": 210, "WS": 211, "SM": 212, "ST": 213, "SA": 214,
+    "SN": 215, "RS": 216, "SC": 217, "SL": 218, "SB": 219, "SO": 220,
+    "ZA": 221, "GS": 222, "LK": 223, "SD": 224, "SR": 225, "SJ": 226,
+    "SZ": 227, "SY": 228, "TJ": 229, "TZ": 230, "TL": 231, "TG": 232,
+    "TK": 233, "TO": 234, "TT": 235, "TN": 236, "TM": 237, "TC": 238,
+    "TV": 239, "UG": 240, "UA": 241, "UM": 242, "UY": 243, "VU": 244,
+    "VE": 245, "VN": 246, "VG": 247, "VI": 248, "WF": 249, "EH": 250,
+    "ZM": 252, "ZW": 253, "AE": 254, "CUW": 258, "SXM": 259,
+    "BES": 260, "BL": 261
+}
 ORDER_DATE_ARGUMENTS = {
     'creation': 'created',
     'change': 'updated',
@@ -79,7 +126,7 @@ def get_route(domain: str) -> str:
             domain [String]     -   Specifies the type of route for the request
                                     {item/order}
 
-        Result:
+        Return:
             [String]
     """
     if re.match(r'order', domain.lower()):
@@ -91,8 +138,48 @@ def get_route(domain: str) -> str:
     return ''
 
 
-def build_date_request_query(date_range: dict, date_type: str,
-                             **kwargs) -> str:
+def get_language(lang: str) -> int:
+    """
+        Use fixed mappings to get the associated PlentyMarkets ID for a
+        country abbreviation.
+
+        Parameter:
+            lang [str]          -   Country abbreviation
+
+        Return:
+            [int]               -   ID from Plentymarkets
+    """
+    try:
+        return VALID_COUNTRY_MAP[lang.upper()]
+    except KeyError:
+        print(f"ERROR: invalid country abbreviation: {lang}")
+        return -1
+
+
+def build_request_query(elements: list) -> str:
+    """
+        Combine different query elements and check the query format.
+
+        Parameter:
+            elements [list]     -   List of strings containing one or more
+                                    query sub-elements
+
+        Return:
+            [str]               -   Full query
+    """
+    query = ''
+    query = query.join(elements)
+
+    # Set the first character to '?' and search for invalid occurences
+    if re.search(r'\?', query):
+        print(f"WARNING: {query} -> found a '?' at position > 0")
+    if query:
+        query = '?' + query[1:]
+
+    return query
+
+
+def build_query_date(date_range: dict, date_type: str) -> str:
     """
         Create a query for the API endpoint, with valid values from the
         PlentyMarkets API documentation:
@@ -109,9 +196,9 @@ def build_date_request_query(date_range: dict, date_type: str,
                                     (use `build_date_range`)
             date_type [String]  -   Identifier for the type of date range
                                     {Creation, Payment, Change, Delivery}
-            (Optional)
-            additional [List]   -   List of additional query arguments
-                                    used with `&with=`
+
+        Return:
+            [str]               -   Date range in query format
     """
     query = ''
     if not date_range or not date_type:
@@ -121,15 +208,30 @@ def build_date_request_query(date_range: dict, date_type: str,
         print(f"ERROR: Invalid date type for query creation: {date_type}")
         return ''
     date_type = ORDER_DATE_ARGUMENTS[date_type.lower()]
-    query += str(f"?{date_type}AtFrom={date_range['start']}")
+    query += str(f"&{date_type}AtFrom={date_range['start']}")
     query += str(f"&{date_type}AtTo={date_range['end']}")
-    if 'additional' in kwargs.keys():
-        for argument in kwargs['additional']:
-            query += str(f"&with[]={argument}")
-    if 'refine' in kwargs.keys():
-        for key, item in kwargs['refine'].items():
-            if key in VALID_REFINE_KEYS:
-                query += str(f"&{key}={item}")
+    return urllib.parse.quote(query, safe='?,&,=')
+
+
+def build_query_attributes(domain: str,
+                           refine: dict = None,
+                           additional: list = None) -> str:
+    """
+            refine
+            additional [List]   -   List of additional query arguments
+                                    used with `&with=`
+    """
+    query = ''
+    if refine is None and additional is None:
+        return ''
+    for key, item in refine.items():
+        if key in VALID_ORDER_REFINE_KEYS and domain.lower() == 'orders':
+            query += str(f"&{key}={item}")
+        elif key in VALID_ITEM_REFINE_KEYS and domain.lower() == 'items':
+            query += str(f"&{key}={item}")
+    for argument in additional:
+        query += str(f"&with={argument}")
+
     return urllib.parse.quote(query, safe='?,&,=')
 
 
@@ -248,6 +350,32 @@ def build_date_range(start: str, end: str) -> dict:
     if not w3c_start or not w3c_end:
         return None
     return {'start': w3c_start, 'end': w3c_end}
+
+
+def date_to_timestamp(date: str) -> int:
+    """
+        Parse a date object in to a unix timestamp.
+
+        Parameter:
+            date [str]      -   date as function parameter in on of the
+                                following formats:
+                                    YYYY-MM-DD
+                                    YYYY-MM-DDTHH:MM
+                                    YYYY-MM-DDTHH:MM:SS+UTC-OFFSET
+
+        Return:
+            [int]           -   Unix timestamp since 1970-01-01
+    """
+    # Check if the date starts with anything else but the year
+    first_number = re.search(r'^\d{2,}(?=\D)', date)
+    if first_number is not None:
+        if int(first_number.group(0)) < 2000:
+            return -1
+    try:
+        date_obj = dateutil.parser.parse(date)
+    except dateutil.parser._parser.ParserError:
+        return -1
+    return int(time.mktime(date_obj.timetuple()))
 
 
 def get_temp_creds() -> dict:

tests/test_utils.py
@@ -2,12 +2,13 @@ import pytest
 
 from plenty_api.utils import (
     get_route, build_endpoint, check_date_range, parse_date, build_date_range,
-    get_utc_offset, build_date_request_query, create_vat_mapping
+    get_utc_offset, build_query_date, create_vat_mapping, date_to_timestamp,
+    get_language
 )
 
 
 @pytest.fixture
-def sample_date_ranges():
+def sample_date_ranges() -> list:
     samples = [
         {'start': '2020-09-14T08:00:00+02:00',  # Normal date => CORRECT
          'end': '2020-09-15T08:00:00+02:00'},
@@ -22,7 +23,7 @@ def sample_date_ranges():
 
 
 @pytest.fixture
-def sample_input_date():
+def sample_input_date() -> list:
     samples = [
         '2020-09-14',
         '14-09-2020',
@@ -36,7 +37,7 @@ def sample_input_date():
 
 
 @pytest.fixture
-def expected_date():
+def expected_date() -> list:
     expected = [
         str(f'2020-09-14T00:00:00+{get_utc_offset()}'),
         str(f'2020-09-14T00:00:00+{get_utc_offset()}'),
@@ -50,7 +51,7 @@ def expected_date():
 
 
 @pytest.fixture
-def sample_date_range_input():
+def sample_date_range_input() -> list:
     samples = [
         {'start': '2020-09-14', 'end': '2020-09-15'},
         {'start': '2020-09-14', 'end': '2020-09-13'},
@@ -64,7 +65,7 @@ def sample_date_range_input():
 
 
 @pytest.fixture
-def expected_date_range():
+def expected_date_range() -> list:
     expected = [
         {'start': str(f'2020-09-14T00:00:00+{get_utc_offset()}'),
          'end': str(f'2020-09-15T00:00:00+{get_utc_offset()}')},
@@ -81,48 +82,34 @@ def expected_date_range():
 
 
 @pytest.fixture
-def sample_query_data():
+def sample_query_data() -> list:
     samples = [
         {'date_range': {'start': '2020-09-14T08:00:00+02:00',
                         'end': '2020-09-14T10:00:30+02:00'},
-         'date_type': 'Creation',
-         'additional': ['documents'],
-         'refine': {}},
+         'date_type': 'Creation'},
         {'date_range': {'start': '2020-09-14T08:00:00+02:00',
                         'end': '2020-09-14T10:00:30+02:00'},
-         'date_type': 'Payment',
-         'additional': ['documents', 'comments'],
-         'refine': {'orderType': '1,4', 'referrerId': '1'}},
+         'date_type': 'Payment'},
         {'date_range': {'start': '2020-09-14T08:00:00+02:00',
                         'end': '2020-09-14T10:00:30+02:00'},
-         'date_type': 'Change',
-         'additional': ['shippingPackages'],
-         'refine': {'countryId': '1'}},
+         'date_type': 'Change'},
         {'date_range': {'start': '2020-09-14T08:00:00+02:00',
                         'end': '2020-09-14T10:00:30+02:00'},
-         'date_type': 'Delivery',
-         'additional': ['documents'],
-         'refine': {}},
+         'date_type': 'Delivery'},
         {'date_range': {},
-         'date_type': 'Creation',
-         'additional': ['documents'],
-         'refine': {}},
+         'date_type': 'Creation'},
         {'date_range': {'start': '2020-09-14T08:00:00+02:00',
                         'end': '2020-09-14T10:00:30+02:00'},
-         'date_type': '',
-         'additional': ['documents'],
-         'refine': {}},
+         'date_type': ''},
         {'date_range': {'start': '2020-09-14T08:00:00+02:00',
                         'end': '2020-09-14T10:00:30+02:00'},
-         'date_type': 'Creation',
-         'additional': '',
-         'refine': {}}
+         'date_type': 'Creation'}
     ]
     return samples
 
 
 @pytest.fixture
-def sample_vat_data():
+def sample_vat_data() -> list:
     samples = [
         [
             {
@@ -165,30 +152,36 @@ def sample_vat_data():
 
 
 @pytest.fixture
-def expected_query():
+def expected_date_query() -> list:
     expected = [
-        '?createdAtFrom=2020-09-14T08%3A00%3A00%2B02%3A00' +
-        '&createdAtTo=2020-09-14T10%3A00%3A30%2B02%3A00' +
-        '&with%5B%5D=documents',
-        '?paidAtFrom=2020-09-14T08%3A00%3A00%2B02%3A00' +
-        '&paidAtTo=2020-09-14T10%3A00%3A30%2B02%3A00' +
-        '&with%5B%5D=documents&with%5B%5D=comments' +
-        '&orderType=1,4&referrerId=1',
-        '?updatedAtFrom=2020-09-14T08%3A00%3A00%2B02%3A00' +
-        '&updatedAtTo=2020-09-14T10%3A00%3A30%2B02%3A00' +
-        '&with%5B%5D=shippingPackages&countryId=1',
-        '?outgoingItemsBookedAtFrom=2020-09-14T08%3A00%3A00%2B02%3A00' +
-        '&outgoingItemsBookedAtTo=2020-09-14T10%3A00%3A30%2B02%3A00' +
-        '&with%5B%5D=documents',
+        '&createdAtFrom=2020-09-14T08%3A00%3A00%2B02%3A00' +
+        '&createdAtTo=2020-09-14T10%3A00%3A30%2B02%3A00',
+        '&paidAtFrom=2020-09-14T08%3A00%3A00%2B02%3A00' +
+        '&paidAtTo=2020-09-14T10%3A00%3A30%2B02%3A00',
+        '&updatedAtFrom=2020-09-14T08%3A00%3A00%2B02%3A00' +
+        '&updatedAtTo=2020-09-14T10%3A00%3A30%2B02%3A00',
+        '&outgoingItemsBookedAtFrom=2020-09-14T08%3A00%3A00%2B02%3A00' +
+        '&outgoingItemsBookedAtTo=2020-09-14T10%3A00%3A30%2B02%3A00',
         '',
         '',
-        '?createdAtFrom=2020-09-14T08%3A00%3A00%2B02%3A00' +
+        '&createdAtFrom=2020-09-14T08%3A00%3A00%2B02%3A00' +
         '&createdAtTo=2020-09-14T10%3A00%3A30%2B02%3A00'
     ]
     return expected
 
 
-def test_get_route():
+@pytest.fixture
+def expected_query_attributes() -> list:
+    expected = [
+        '&with%5B%5D=documents',
+        '&with%5B%5D=documents&with%5B%5D=comments&orderType=1,4&referrerId=1',
+        '&with%5B%5D=shippingPackages&countryId=1',
+        '&with%5B%5D=documents'
+    ]
+    return expected
+
+
+def test_get_route() -> None:
     sample_data = ['order', 'item', 'ITEMS', 'oRdErS', 'wrong', '']
     result = []
     expected = ['/rest/orders', '/rest/items', '/rest/items', '/rest/orders',
@@ -200,7 +193,7 @@ def test_get_route():
     assert expected == result
 
 
-def test_build_endpoint():
+def test_build_endpoint() -> None:
     sample_data = [
         {'url': 'https://test.plentymarkets-cloud01.com',
          'route': '/rest/orders',
@@ -236,7 +229,7 @@ def test_build_endpoint():
     assert expected == result
 
 
-def test_check_date_range(sample_date_ranges):
+def test_check_date_range(sample_date_ranges: list) -> None:
     expected = [True, False, True, False]
     result = []
 
@@ -246,7 +239,8 @@ def test_check_date_range(sample_date_ranges):
     assert expected == result
 
 
-def test_parse_date(sample_input_date, expected_date):
+def test_parse_date(sample_input_date: list,
+                    expected_date: list) -> None:
     result = []
 
     for sample in sample_input_date:
@@ -255,7 +249,8 @@ def test_parse_date(sample_input_date, expected_date):
     assert expected_date == result
 
 
-def test_build_date_range(sample_date_range_input, expected_date_range):
+def test_build_date_range(sample_date_range_input: list,
+                          expected_date_range: list) -> None:
     result = []
     for sample in sample_date_range_input:
         result.append(build_date_range(start=sample['start'],
@@ -264,17 +259,15 @@ def test_build_date_range(sample_date_range_input, expected_date_range):
     assert expected_date_range == result
 
 
-def test_build_date_request_query(sample_query_data, expected_query):
+def test_build_query_date(sample_query_data: list,
+                          expected_date_query: list) -> None:
     result = []
 
     for sample in sample_query_data:
-        result.append(build_date_request_query(date_range=sample['date_range'],
-                                               date_type=sample['date_type'],
-                                               additional=sample['additional'],
-                                               refine=sample['refine'])
-                      )
+        result.append(build_query_date(date_range=sample['date_range'],
+                                       date_type=sample['date_type']))
 
-    assert expected_query == result
+    assert expected_date_query == result
 
 
 def test_create_vat_mapping(sample_vat_data: list) -> None:
@@ -297,5 +290,30 @@ def test_create_vat_mapping(sample_vat_data: list) -> None:
     for sample in sample_vat_data:
         for sub in subset:
             result.append(create_vat_mapping(data=sample, subset=sub))
+
+    assert expected == result
+
+
+def test_date_to_timestamp() -> None:
+    samples = ['2020-08-01', '2020-08-01T15:00', '2020-08-01T15:00:00+02:00',
+               '01-08-2020', '2020.08.01', 'abc', '']
+    loc = int(get_utc_offset()[0:3].strip(':'))*3600
+    expected = [1596232800, 1596286800, 1596290400,
+                -1, 1596232800, -1, -1]
+    result = []
+
+    for sample in samples:
+        result.append(date_to_timestamp(date=sample))
+
+    assert expected == result
+
+
+def test_get_language() -> None:
+    samples = ['de', 'GB', 'fR', 'Greece', '12', '']
+    expected = [1, 12, 10, -1, -1, -1]
+    result = []
+
+    for sample in samples:
+        result.append(get_language(lang=sample))
 
     assert expected == result

commit 6b80c78298e755a87f211ac69363299dc054bd13

[FIX] directly return response if it’s already a list
The previous logic assumed that the response of `__plenty_api_request`
is always a dictionary. But it can also be a list.

Fixes: 5f8ab60f Feature & Refactor: Item data & generalize logic

Signed-off-by: Peter Hahn <peter.hahn@initos.com>
Reviewed-by: Sebastian Fricke <sebastian.fricke@posteo.net>
Signed-off-by: Sebastian Fricke <sebastian.fricke@posteo.net>

plenty_api/api.py
@@ -430,7 +430,8 @@ class PlentyApi():
         if not response:
             return None
 
-        if isinstance(response, dict) and 'error' in response.keys():
+        if ((isinstance(response, dict) and 'error' in response.keys()) or
+                isinstance(response, list)):
             return response
 
         entries = response['entries']