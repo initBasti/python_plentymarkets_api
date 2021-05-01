initBasti/python_plentymarkets_api

commit 1818bbf09128496b2ffb9e1bda13b5d42b2e0660

Refactor: Generalize the sanity check of parameter
Reduce code repition by generalizing the sanity check of major parameter
groups for multiple requests. Add tests for that function.
Additionally align function description style.

Signed-off-by: Sebastian Fricke <sebastian.fricke.linux@gmail.com>

plenty_api/utils.py
@@ -34,10 +34,10 @@ def create_vat_mapping(data: list, subset: list = None) -> dict:
         restrict the mapping to a subset if given.
 
         Parameter:
-            data [List]         -   Response JSON data from /rest/vat request
+            data    [list]      -   Response JSON data from /rest/vat request
 
         Return:
-            [Dict]
+                    [dict]
     """
     mapping = {}
     if not data or not isinstance(data[0], dict):
@@ -62,10 +62,10 @@ def shrink_price_configuration(data: dict) -> dict:
         date information and other additional mappings.
 
         Parameter:
-            data [dict]         -   The response JSON dictionary
+            data    [dict]      -   The response JSON dictionary
 
         Return:
-            [dict]
+                    [dict]
     """
     configuration: dict = {
         'id': 0,
@@ -111,11 +111,11 @@ def get_route(domain: str) -> str:
         Use fixed mappings to determine the correct route for the endpoint.
 
         Parameter:
-            domain [String]     -   Specifies the type of route for the request
-                                    {item/order}
+            domain  [str]       -   Specifies the type of route for the request
+                                    {item/order/..}
 
         Return:
-            [String]
+                    [str]
     """
     for valid_domain in constants.VALID_DOMAINS:
         if re.match(valid_domain, domain.lower()):
@@ -129,16 +129,67 @@ def get_language(lang: str) -> int:
         country abbreviation.
 
         Parameter:
-            lang [str]          -   Country abbreviation
+            lang    [str]       -   Country abbreviation
 
         Return:
-            [int]               -   ID from Plentymarkets
+                    [int]       -   ID from Plentymarkets
     """
     try:
         return constants.VALID_COUNTRY_MAP[lang.upper()]
     except KeyError:
         print(f"ERROR: invalid country abbreviation: {lang}")
         return -1
+
+
+def sanity_check_parameter(domain: str,
+                           query: dict,
+                           refine: dict = None,
+                           additional: list = None,
+                           lang: str = ''):
+    """
+        Build the query dictionary, while checking for invalid arguments
+        and removing them.
+
+        Parameter:
+            domain     [str]    -   specifies the type of route for the request
+                                    {item/order/..}
+            query      [dict]   -   Dictionary used for the params field
+                                    for the requests module.
+            refine     [dict]   -   Filters for the request
+            additional [list]   -   additional elements for the response body
+            lang       [str]    -   Name of the language for product texts
+
+        Return:
+                       [dict]   -   updated query
+    """
+    if domain not in constants.VALID_DOMAINS:
+        print(f"ERROR: invalid domain name {domain}")
+        return {}
+
+    if refine:
+        invalid_keys = set(refine.keys()).difference(
+            constants.VALID_REFINE_KEYS[domain])
+        if invalid_keys:
+            print(f"Invalid refine argument key removed: {invalid_keys}")
+            for invalid_key in invalid_keys:
+                refine.pop(invalid_key, None)
+        if refine:
+            query.update(refine)
+
+    if additional:
+        invalid_values = set(additional).difference(
+            constants.VALID_ADDITIONAL_VALUES[domain])
+        if invalid_values:
+            print(f"Invalid additional argument removed: {invalid_values}")
+            for invalid_value in invalid_values:
+                additional.remove(invalid_value)
+        if additional:
+            query.update({'with': additional})
+
+    if lang:
+        query.update({'lang': get_language(lang=lang)})
+
+    return query
 
 
 def build_query_date(date_range: dict, date_type: str) -> dict:
@@ -154,13 +205,13 @@ def build_query_date(date_range: dict, date_type: str) -> dict:
             Delivery : {outgoingItemsBookedAtFrom & outgoingItemsBookedAtTo}
 
         Parameter:
-            date_range [Dict]   -   Start & End date in W3C date format
+            date_range  [dict]  -   Start & End date in W3C date format
                                     (use `build_date_range`)
-            date_type [String]  -   Identifier for the type of date range
+            date_type   [str]   -   Identifier for the type of date range
                                     {Creation, Payment, Change, Delivery}
 
         Return:
-            [dict]               -   Date range in python dictionary
+                        [dict]  -   Date range in python dictionary
     """
     query = {}
     if not date_range or not date_type:
@@ -184,12 +235,12 @@ def build_endpoint(url: str, route: str, path: str = '') -> str:
         and having the correcting HTTP encoding for special signs.
 
         Parameter:
-            url [String]        -   Base url of the plentymarkets API
-            route [String]      -   Route part endpoint (e.g. /rest/items)
-            path [String]       -   Sub route part (e.g. /{item_id}/images)
+            url     [str]       -   Base url of the plentymarkets API
+            route   [str]       -   Route part endpoint (e.g. /rest/items)
+            path    [str]       -   Sub route part (e.g. /{item_id}/images)
 
         Parameter:
-            [String]            -   complete endpoint
+                    [str]       -   complete endpoint
     """
     if not re.search(r'https://.*.plentymarkets-cloud01.com', url):
         print("ERROR: invalid URL, need: {https://*.plentymarkets-cloud01.com")
@@ -212,7 +263,7 @@ def get_utc_offset() -> str:
         and UTC and return a string with the format "02:00"
 
         Return:
-            [String]
+                    [str]
     """
     current = datetime.datetime.now(datetime.timezone.utc).astimezone()
     offset = current.tzinfo.utcoffset(None)
@@ -225,10 +276,10 @@ def check_date_range(date_range: dict) -> bool:
         Check if the user specified date range is a valid range in the past.
 
         Parameter:
-            date_range [Dict]   -   start and end date
+            date_range [dict]   -   start and end date
 
         Return:
-            [Bool]
+                       [bool]
     """
     now = datetime.datetime.now().astimezone()
     try:
@@ -259,10 +310,11 @@ def parse_date(date: str) -> str:
         the PlentyMarkets API.
 
         Parameter:
-            date [String]   -   user supplied string with the original date.
+            date    [str]       -   user supplied string with the
+                                    original date.
 
         Return:
-            [String]
+                    [str]
     """
     try:
         date = dateutil.parser.parse(date)
@@ -280,11 +332,11 @@ def build_date_range(start: str, end: str) -> dict:
         Create a range of 2 dates in the W3C dateformat.
 
         Parameter:
-            start [String]  -   user supplied string with the start date
-            end [String]    -   user supplied string with the end date
+            start   [str]       -   user supplied string with the start date
+            end     [str]       -   user supplied string with the end date
 
         Return:
-            [Dict]/None
+                    [dict]/None
     """
     w3c_start = parse_date(date=start)
     w3c_end = parse_date(date=end)
@@ -298,14 +350,14 @@ def date_to_timestamp(date: str) -> int:
         Parse a date object in to a unix timestamp.
 
         Parameter:
-            date [str]      -   date as function parameter in on of the
-                                following formats:
+            date    [str]       -   date as function parameter in on of the
+                                    following formats:
                                     YYYY-MM-DD
                                     YYYY-MM-DDTHH:MM
                                     YYYY-MM-DDTHH:MM:SS+UTC-OFFSET
 
         Return:
-            [int]           -   Unix timestamp since 1970-01-01
+                    [int]       -   Unix timestamp since 1970-01-01
     """
     # Check if the date starts with anything else but the year
     first_number = re.search(r'^\d{2,}(?=\D)', date)
@@ -331,32 +383,32 @@ def get_temp_creds() -> dict:
     return {'username': username, 'password': password}
 
 
-def new_keyring_creds(kr: object) -> dict:
+def new_keyring_creds(keyring: object) -> dict:
     """
         Get the credentials for the API from the user and store them into
         a system wide keyring
 
         Parameter:
-            kr [CredentialManager object]
+            keyring [CredentialManager object]
         Return:
-            [Dict]  - containing username and password
+                    [dict]      - containing username and password
     """
-    kr.set_credentials()
-    return kr.get_credentials()
+    keyring.set_credentials()
+    return keyring.get_credentials()
 
 
-def update_keyring_creds(kr: object) -> dict:
+def update_keyring_creds(keyring: object) -> dict:
     """
         Delete the current content of the keyring and get new credentials
         for the API from the user, store them into the keyring
 
         Parameter:
-            kr [CredentialManager object]
+            keyring [CredentialManager object]
         Return:
-            [Dict]  - containing username and password
+                    [dict]      - containing username and password
     """
-    kr.delete_credentials()
-    return new_keyring_creds(kr=kr)
+    keyring.delete_credentials()
+    return new_keyring_creds(keyring=keyring)
 
 
 def build_login_token(response_json: dict) -> str:

plenty_api/api.py
@@ -294,17 +294,11 @@ class PlentyApi():
 
         query = utils.build_query_date(date_range=date_range,
                                        date_type=date_type)
-        if refine:
-            invalid_keys = set(refine.keys()).difference(
-                constants.VALID_ORDER_REFINE_KEYS)
-            if invalid_keys:
-                print(f"Invalid refine argument key removed: {invalid_keys}")
-                for invalid_key in invalid_keys:
-                    refine.pop(invalid_key, None)
-            query.update(refine)
 
-        if additional:
-            query.update({'with': additional})
+        query = utils.sanity_check_parameter(domain='order',
+                                             query=query,
+                                             refine=refine,
+                                             additional=additional)
 
         orders = self.__repeat_get_request_for_all_records(domain='orders',
                                                            query=query)
@@ -421,24 +415,15 @@ class PlentyApi():
         items = None
         query = {}
 
-        if refine:
-            invalid_keys = set(refine.keys()).difference(
-                constants.VALID_ITEM_REFINE_KEYS)
-            if invalid_keys:
-                print(f"Invalid refine argument key removed: {invalid_keys}")
-                for invalid_key in invalid_keys:
-                    refine.pop(invalid_key, None)
-            query.update(refine)
-
-        if additional:
-            query.update({'with': additional})
+        query = utils.sanity_check_parameter(domain='item',
+                                             query=query,
+                                             refine=refine,
+                                             additional=additional,
+                                             lang=lang)
 
         if last_update:
             query.update({'updatedBetween': utils.date_to_timestamp(
                          date=last_update)})
-
-        if lang:
-            query.update({'lang': utils.get_language(lang=lang)})
 
         items = self.__repeat_get_request_for_all_records(domain='items',
                                                           query=query)
@@ -476,20 +461,11 @@ class PlentyApi():
         variations = None
         query = {}
 
-        if refine:
-            invalid_keys = set(refine.keys()).difference(
-                constants.VALID_VARIATION_REFINE_KEYS)
-            if invalid_keys:
-                print(f"Invalid refine argument key removed: {invalid_keys}")
-                for invalid_key in invalid_keys:
-                    refine.pop(invalid_key, None)
-            query.update(refine)
-
-        if additional:
-            query.update({'with': additional})
-
-        if lang:
-            query.update({'lang': utils.get_language(lang=lang)})
+        query = utils.sanity_check_parameter(domain='variation',
+                                             query=query,
+                                             refine=refine,
+                                             additional=additional,
+                                             lang=lang)
 
         variations = self.__repeat_get_request_for_all_records(
             domain='variations', query=query)

tests/test_utils.py
@@ -4,7 +4,7 @@ import requests
 from plenty_api.utils import (
     get_route, build_endpoint, check_date_range, parse_date, build_date_range,
     get_utc_offset, build_query_date, create_vat_mapping, date_to_timestamp,
-    get_language, shrink_price_configuration
+    get_language, shrink_price_configuration, sanity_check_parameter
 )
 
 
@@ -79,17 +79,17 @@ def sample_price_response() -> list:
                            'updatedAt': '1990-07-09T15:33:46+02:00'}],
             'createdAt': '1990-09-05 13:24:53',
             'currencies': [{'createdAt': '1990-07-09T15:33:46+02:00',
-                              'currency': 'EUR',
-                              'salesPriceId': 1,
-                              'updatedAt': '1990-07-09T15:33:46+02:00'},
-                             {'createdAt': '1990-07-09T15:33:46+02:00',
-                              'currency': 'GBP',
-                              'salesPriceId': 1,
-                              'updatedAt': '1990-07-09T15:33:46+02:00'}],
+                            'currency': 'EUR',
+                            'salesPriceId': 1,
+                            'updatedAt': '1990-07-09T15:33:46+02:00'},
+                           {'createdAt': '1990-07-09T15:33:46+02:00',
+                            'currency': 'GBP',
+                            'salesPriceId': 1,
+                            'updatedAt': '1990-07-09T15:33:46+02:00'}],
             'customerClasses': [{'createdAt': '1990-07-09T15:33:46+02:00',
-                                   'customerClassId': -1,
-                                   'salesPriceId': 1,
-                                   'updatedAt': '1990-07-09T15:33:46+02:00'}],
+                                 'customerClassId': -1,
+                                 'salesPriceId': 1,
+                                 'updatedAt': '1990-07-09T15:33:46+02:00'}],
             'id': 1,
             'interval': 'none',
             'isCustomerPrice': False,
@@ -228,6 +228,47 @@ def sample_vat_data() -> list:
 
 
 @pytest.fixture
+def sample_sanity_check_parameter() -> list:
+    samples = [
+        {
+            'domain': 'manufacturer',
+            'query': {},
+            'refine': {},
+            'additional': [],
+            'lang': ''
+        },
+        {
+            'domain': 'variation',
+            'query': {},
+            'refine': {'id': 1234, 'itemId': 10234},
+            'additional': ['properties', 'stock'],
+            'lang': 'at'
+        },
+        {
+            'domain': 'item',
+            'query': {},
+            'refine': {'id': 10234, 'wrong': 'wrong'},
+            'additional': ['variations'],
+            'lang': ''
+        },
+        {
+            'domain': 'order',
+            'query': {'orderType': 1},
+            'refine': {'wrong': 'wrong'},
+            'additional': ['wrong'],
+            'lang': ''
+        },
+        {
+            'domain': 'wrong',
+            'query': {'shall': 'not_appear'},
+            'refine': {'should': 'be_insignificant'},
+            'additional': ['nono'],
+            'lang': 'de'
+        }
+    ]
+    return samples
+
+@pytest.fixture
 def expected_date_query() -> list:
     expected = [
         'createdAtFrom=2020-09-14T08%3A00%3A00%2B02%3A00' +
@@ -253,6 +294,28 @@ def expected_query_attributes() -> list:
         '&with%5B%5D=documents&with%5B%5D=comments&orderType=1,4&referrerId=1',
         '&with%5B%5D=shippingPackages&countryId=1',
         '&with%5B%5D=documents'
+    ]
+    return expected
+
+
+@pytest.fixture
+def expected_sanity_check_query() -> list:
+    expected = [
+        # empty query
+        {},
+        # domain: variation
+        # valid domain, 2 refine, 2 additional, lang all valid arguments
+        {'id': 1234, 'itemId': 10234,
+         'with': ['properties', 'stock'], 'lang': 2},
+        # domain: item
+        # valid domain 2 refine, 1 additional, 1 invalid refine
+        {'id': 10234, 'with': ['variations']},
+        # domain: order
+        # all invalid arguments, but preexisting query
+        {'orderType': 1},
+        # domain: wrong
+        # invalid domain
+        {}
     ]
     return expected
 
@@ -397,4 +460,18 @@ def test_shrink_price_configuration(sample_price_response: dict,
     for sample in sample_price_response:
         result.append(shrink_price_configuration(data=sample))
 
-    assert result == expected_prices
+    assert expected_prices == result
+
+
+def test_sanity_check_parameter(sample_sanity_check_parameter: dict,
+                                expected_sanity_check_query: dict) -> None:
+    result = []
+
+    for sample in sample_sanity_check_parameter:
+        result.append(sanity_check_parameter(domain=sample['domain'],
+                                             query=sample['query'],
+                                             refine=sample['refine'],
+                                             additional=sample['additional'],
+                                             lang=sample['lang']))
+
+    assert expected_sanity_check_query == result

commit 92300c6b92ecd15cd4bb7c4554758de8ec212d3c

Fix: Enable handling empty queries during sanity check
When the `query` parameter was None, the function failed, fix this by
checking for that condition in the beginning.

Fixes: 1818bbf0 Refactor: Generalize the sanity check of parameter

Signed-off-by: Sebastian Fricke <sebastian.fricke@posteo.net>

plenty_api/utils.py
@@ -214,6 +214,9 @@ def sanity_check_parameter(domain: str,
     Return:
                         [dict]   -   updated query
     """
+    if not query:
+        query = {}
+
     if domain not in constants.VALID_DOMAINS:
         logging.error(f"Invalid domain name {domain}")
         return {}