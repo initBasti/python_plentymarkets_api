initBasti/python_plentymarkets_api

commit 6e5c84b9e586616068fb7f55079c641230914d35

Allow plain text password for non-interactive use by client software
Add a fourth login method besides the current methods:
- Adding the password and user to the system keyring
- Prompting within the terminal for user and password without storing it
- Passing the username per parameter at object creation and read the
  password from a GPG encrypted file on the system

The new method allows the user to pass username and password over the
parameters at object creation.

Signed-off-by: Peter Hahn <peter.hahn@initos.com>
Reviewed-by: Sebastian Fricke <sebastian.fricke@posteo.net>

plenty_api/api.py
@@ -227,7 +227,7 @@ class PlentyApi():
 
     def __init__(self, base_url: str, use_keyring: bool = True,
                  data_format: str = 'json', debug: bool = False,
-                 username: str = '', password: str = ''):
+                 username: str = '', password: str = '', use_gpg: bool = True):
         """
             Initialize the object and directly authenticate to the API to get
             the bearer token.
@@ -243,9 +243,10 @@ class PlentyApi():
                                         about the request URL and parameters
                 username    [str]   -   skip the keyring and directly enter
                                         the username to the REST-API
-                password    [str]   -   path to a gpg-encrypted file that
-                                        contains the key.
-
+                password    [str]   -   password string or path to a gpg-encrypted
+                                        file that contains the key.
+                use_gpg     [bool]  -   password is path to a gpg-encrypted
+                                        file that contains the key.
         """
         self.url = base_url
         self.keyring = plenty_api.keyring.CredentialManager()
@@ -256,11 +257,11 @@ class PlentyApi():
             self.data_format = 'json'
         self.creds = {'Authorization': ''}
         logged_in = self.__authenticate(
-            persistent=use_keyring, user=username, pw=password)
+            persistent=use_keyring, user=username, pw=password, use_gpg=use_gpg)
         if not logged_in:
             raise RuntimeError('Authentication failed')
 
-    def __authenticate(self, persistent: bool, user: str, pw: str):
+    def __authenticate(self, persistent: bool, user: str, pw: str, use_gpg: bool):
         """
             Get the bearer token from the PlentyMarkets API.
             There are three possible methods:
@@ -294,20 +295,23 @@ class PlentyApi():
         elif not persistent and not (user and pw):
             creds = utils.get_temp_creds()
         elif user and pw:
-            gpg = gnupg.GPG()
-            try:
-                with open(pw, 'rb') as pw_file:
-                    decrypt_pw = gpg.decrypt_file(pw_file)
-            except FileNotFoundError as err:
-                print("ERROR Login to API failed: Provided gpg file is not "
-                      f"valid\n=> {err}")
-                return False
-            if not decrypt_pw:
-                print("ERROR Login to API failed: Decryption of password file"
-                      " failed.")
-                return False
-            password = decrypt_pw.data.decode('utf-8').strip('\n')
-            creds = {'username': user, 'password': password}
+            if use_gpg:
+                gpg = gnupg.GPG()
+                try:
+                    with open(pw, 'rb') as pw_file:
+                        decrypt_pw = gpg.decrypt_file(pw_file)
+                except FileNotFoundError as err:
+                    logging.error("Login to API failed: Provided gpg file is not "
+                                  f"valid\n=> {err}")
+                    return False
+                if not decrypt_pw:
+                    logging.error("Login to API failed: Decryption of password"
+                                  " file failed.")
+                    return False
+                password = decrypt_pw.data.decode('utf-8').strip('\n')
+                creds = {'username': user, 'password': password}
+            else:
+                creds = {'username': user, 'password': pw}
 
         endpoint = self.url + '/rest/login'
         response = requests.post(endpoint, params=creds)

commit 90d9838a3091239c7a4a1aefbf0158b4310ee212

Fix: api.py: Add missing documentation for new parameter
The new `use_gpg` parameter for the `__authenticate` method was not
properly documentated. Append the method documentation with a
description of the purpose of the parameter.

Fixes: 6e5c84b Allow plain text password for non-interactive use by client software

Signed-off-by: Sebastian Fricke <sebastian.fricke@posteo.net>

plenty_api/api.py
@@ -277,7 +277,10 @@ class PlentyApi():
 
             + Provide username as an argument and get the password
                 from a GnuPG encrypted file at a specified path.
-                [user TRUE and pw TRUE]
+                [user TRUE and pw TRUE and use_gpg TRUE]
+
+            + Provide username and the password as arguments
+                [user TRUE and pw TRUE an use_gpg FALSE]
 
         Parameter:
             persistent  [bool]  -   Permanent or temporary credential storage
@@ -285,6 +288,9 @@ class PlentyApi():
                                     username to the REST-API
             pw          [str]   -   path to a gpg-encrypted file that contains
                                     the key.
+            use_gpg     [bool]  -   Indicates if @pw is a password
+                                    string(False) or a file path to a
+                                    gpg encrypted file containing the password
 
         Return:
                         [bool]