initBasti/python_plentymarkets_api

commit f714388f2dff6fb43bd9c5c0c3eec51b9ae699d0

Add more GET routes
Add additional GET routes to pull stock data, storage locations,
warehouses and contacts.
Provide additional public methods to pull storage locations for a
specific variation.

Signed-off-by: Peter Hahn <peter.hahn@initos.com>
Reviewed-by: Sebastian Fricke <sebastian.fricke@posteo.net>
Signed-off-by: Sebastian Fricke <sebastian.fricke@posteo.net>

plenty_api/constants.py
@@ -22,14 +22,33 @@
     https://developers.plentymarkets.com/rest-doc#
 """
 
-VALID_DOMAINS = ['order', 'item', 'variation', 'vat', 'prices', 'manufacturer',
-                 'attribute', 'referrer', 'redistribution']
-VALID_ROUTES = ['/rest/orders', '/rest/items', '/rest/items/variations',
-                '/rest/vat', '/rest/items/sales_prices',
-                '/rest/items/manufacturers', '/rest/items/attributes',
-                '/rest/orders/referrers', '/rest/redistributions']
-DOMAIN_ROUTE_MAP = {VALID_DOMAINS[i]: VALID_ROUTES[i]
-                    for i in range(len(VALID_DOMAINS))}
+VALID_DOMAINS = ['order',
+                 'item',
+                 'variation',
+                 'vat',
+                 'prices',
+                 'manufacturer',
+                 'attribute',
+                 'referrer',
+                 'stockmanagement',
+                 'warehouses',
+                 'contact',
+                 'redistribution'
+                 ]
+VALID_ROUTES = ['/rest/orders',
+                '/rest/items',
+                '/rest/items/variations',
+                '/rest/vat',
+                '/rest/items/sales_prices',
+                '/rest/items/manufacturers',
+                '/rest/items/attributes',
+                '/rest/orders/referrers',
+                '/rest/stockmanagement/stock',
+                '/rest/stockmanagement/warehouses',
+                '/rest/accounts/contacts',
+                '/rest/redistributions'
+                ]
+DOMAIN_ROUTE_MAP = dict(zip(VALID_DOMAINS, VALID_ROUTES))
 
 # Mapping of date_type function parameter value to query parameter
 # the date_type function parameter is supposed to be more descriptive
@@ -67,7 +86,23 @@ VALID_REFINE_KEYS = {
     ],
     'manufacturer': [
         'name'
-    ]
+    ],
+    'stockmanagement': [
+        'variationId',
+    ],
+    'warehouses': [
+        'variationId',
+    ],
+    'contact': [
+        'fullText', 'contactEmail', 'email', 'postalCode', 'plentyId',
+        'externalId', 'number', 'typeId', 'rating', 'createdAtBefore',
+        'createdAtAfter', 'updatedAtBefore', 'updatedAtAfter',
+        'lastOrderAtBefore', 'lastOrderAtAfter', 'with',
+        'newsletterAllowanceAfter', 'newsletterAllowanceBefore',
+        'newsletterAllowance', 'contactId', 'contactAddress', 'countryId',
+        'userId', 'referrerId', 'name', 'nameOrId', 'town', 'privatePhone',
+        'billingAddressId', 'deliveryAddressId', 'tagIds'
+    ],
 }
 
 # Valid additional argument values for GET requests, which are used to

plenty_api/api.py
@@ -812,6 +812,59 @@ class PlentyApi():
                                            query=query,
                                            lang=lang)
 
+    def plenty_api_get_stock(self,
+                             refine: dict = None,
+                             lang: str = ''):
+        return self.plenty_api_generic_get(domain='stockmanagement',
+                                           refine=refine,
+                                           lang=lang)
+
+    def plenty_api_get_storagelocations(self,
+                                        warehouse_id: int,
+                                        refine: dict = None,
+                                        additional: list = None,
+                                        lang: str = ''):
+        return self.plenty_api_generic_get(
+            domain='warehouses',
+            path=f'/{warehouse_id}/stock/storageLocations',
+            refine=refine,
+            additional=additional,
+            lang=lang)
+
+    def plenty_api_get_variation_stock_batches(self, variation_id: int):
+        # get all warehouses for this item
+        refine = {'variationId': variation_id}
+
+        # returns only locations with positive stock
+        stock = self.plenty_api_get_stock(refine=refine)
+        warehouses = [s['warehouseId'] for s in stock]
+
+        # get storage data from everywhere
+        storage_data = [location
+                        for warehouse_id in warehouses
+                        for location in self.plenty_api_get_storagelocations(
+                        warehouse_id, refine=refine)]
+
+        # return ordered by best before date (oldest first)
+        return sorted(storage_data, key=lambda s: s['bestBeforeDate'])
+
+    def plenty_api_get_variation_warehouses(self,
+                                            item_id: int,
+                                            variation_id: int):
+        return self.plenty_api_generic_get(
+            domain='item',
+            path=f'/{item_id}/variations/{variation_id}/variation_warehouses')
+
+    def plenty_api_get_contacts(self,
+                                refine: dict = None,
+                                additional: list = None,
+                                lang: str = ''):
+        return self.plenty_api_generic_get(
+            domain='contact',
+            refine=refine,
+            additional=additional,
+            lang=lang)
+
 # POST REQUESTS
 
     def plenty_api_set_image_availability(self,

commit 8a6ceea8669f7875e0a4e9c071dab7c39106177c

Fix: Add missing valid values for get_storagelocations
The route provides an additional refine and a additional value, which
were missing from the valid values table. Add the missing values.

Fixes: f714388 Add more GET routes

Signed-off-by: Sebastian Fricke <sebastian.fricke@posteo.net>

plenty_api/constants.py
@@ -93,7 +93,7 @@ VALID_REFINE_KEYS = {
         'variationId',
     ],
     'warehouses': [
-        'variationId',
+        'variationId', 'storageLocationId'
     ],
     'contact': [
         'fullText', 'contactEmail', 'email', 'postalCode', 'plentyId',
@@ -139,6 +139,9 @@ VALID_ADDITIONAL_VALUES = {
     'contact': [
         'addresses', 'accounts', 'options', 'orderSummary',
         'primaryBillingAddress', 'contactOrders'
+    ],
+    'warehouses': [
+        'storageLocation'
     ]
 }
 