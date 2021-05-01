initBasti/python_plentymarkets_api

commit 64f479e0705c3422c52c8e3440876be34a21f8a4

[REF] move boiler plate code into generic function
Signed-off-by: Peter Hahn <peter.hahn@initos.com>
Reviewed-by: Sebastian Fricke <sebastian.fricke@posteo.net>
Signed-off-by: Sebastian Fricke <sebastian.fricke@posteo.net>

plenty_api/api.py
@@ -453,6 +453,24 @@ class PlentyApi():
 
         return entries
 
+    def plenty_api_generic_get(self,
+                               domain: str = '',
+                               path: str = '',
+                               refine: dict = None,
+                               additional: list = None,
+                               query: dict = {},
+                               lang: str = ''):
+
+        query = utils.sanity_check_parameter(
+            domain=domain, query=query, refine=refine,
+            additional=additional,lang=lang)
+
+        data = self.__repeat_get_request_for_all_records(
+            domain=domain, path=path, query=query)
+
+        return utils.transform_data_type(
+            data=data, data_format=self.data_format)
+
     def plenty_api_get_orders_by_date(self, start, end, date_type='create',
                                       additional=None, refine=None):
         """
@@ -672,25 +690,13 @@ class PlentyApi():
             Return:
                 [JSON(Dict) / DataFrame] <= self.data_format
         """
-        manufacturers = None
         query = {}
-        query = utils.sanity_check_parameter(domain='manufacturer',
-                                             query=query,
-                                             refine=refine,
-                                             additional=additional)
-
         if last_update:
             query.update({'updatedAt': last_update})
-
-        manufacturers = self.__repeat_get_request_for_all_records(
-            domain='manufacturer', query=query)
-        if isinstance(manufacturers, dict) and 'error' in manufacturers.keys():
-            logging.error(f"GET manufacturers failed with:\n{manufacturers}")
-            return None
-
-        manufacturers = utils.transform_data_type(data=manufacturers,
-                                                  data_format=self.data_format)
-        return manufacturers
+        return self.plenty_api_generic_get(domain='manufacturer',
+                                           query=query,
+                                           refine=refine,
+                                           additional=additional)
 
     def plenty_api_get_referrers(self,
                                  column: str = ''):
@@ -763,28 +769,16 @@ class PlentyApi():
             Return:
                 [JSON(Dict) / DataFrame] <= self.data_format
         """
-        items = None
         query = {}
-
-        query = utils.sanity_check_parameter(domain='item',
-                                             query=query,
-                                             refine=refine,
-                                             additional=additional,
-                                             lang=lang)
-
         if last_update:
             query.update({'updatedBetween': utils.date_to_timestamp(
                          date=last_update)})
 
-        items = self.__repeat_get_request_for_all_records(domain='items',
-                                                          query=query)
-        if isinstance(items, dict) and 'error' in items.keys():
-            logging.error(f"GET items failed with:\n{items}")
-            return None
-
-        items = utils.transform_data_type(data=items,
-                                          data_format=self.data_format)
-        return items
+        return self.plenty_api_generic_get(domain='item',
+                                           query=query,
+                                           refine=refine,
+                                           additional=additional,
+                                           lang=lang)
 
     def plenty_api_get_variations(self,
                                   refine: dict = None,
@@ -810,24 +804,13 @@ class PlentyApi():
             Return:
                 [JSON(Dict) / DataFrame] <= self.data_format
         """
-        variations = None
         query = {}
 
-        query = utils.sanity_check_parameter(domain='variation',
-                                             query=query,
-                                             refine=refine,
-                                             additional=additional,
-                                             lang=lang)
-
-        variations = self.__repeat_get_request_for_all_records(
-            domain='variations', query=query)
-        if isinstance(variations, dict) and 'error' in variations.keys():
-            logging.error(f"GET variations failed with:\n{variations}")
-            return None
-
-        variations = utils.transform_data_type(data=variations,
-                                               data_format=self.data_format)
-        return variations
+        return self.plenty_api_generic_get(domain='variation',
+                                           refine=refine,
+                                           additional=additional,
+                                           query=query,
+                                           lang=lang)
 
 # POST REQUESTS
 

commit 4dfcbeadb9b5812d96656ff75b2f592c6d62b1d0

Fix: Fix warning about dangerous default value
Fix a pylint warning, that warns about the usage of a dangerous default
value '{}'. Use 'None' instead.

Fixes: 64f479e [REF] move boiler plate code into generic function

Signed-off-by: Sebastian Fricke <sebastian.fricke@posteo.net>

plenty_api/api.py
@@ -343,7 +343,7 @@ class PlentyApi():
                                  path: str = '',
                                  refine: dict = None,
                                  additional: list = None,
-                                 query: dict = {},
+                                 query: dict = None,
                                  lang: str = ''):
         """
         Generic wrapper for GET routes that includes basic checks, repeated